8411419fe846e6faed53997abfdc027deca142d1
hct <564117373@qq.com>
2020-04-10 16:32:22 +0800

    update

---
+++ docs/.vuepress/components/custom-action.vue
@@ -0,0 +1,40 @@
+<template>
+  <p id="custom-action">
+    <a class="nav-link action-button" @click="jump('note/basic')">进入笔记</a>
+    <a class="nav-link action-button" @click="jump('CV')">我的简历 →</a>
+  </p>
+</template>
+
+<script>
+export default {
+  methods: {
+    jump(text) {
+      this.$router.push({ path: `/${text}.html` })
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#custom-action {
+  margin: 1.8rem auto;
+  text-align: center;
+
+  .action-button {
+    display: inline-block;
+    font-size: 1.2rem;
+    color: white;
+    background: #3eaf7c;
+    margin 0 1rem
+    padding: 0.8rem 1.6rem;
+    border-radius: 4px;
+    box-sizing: border-box;
+    border-bottom: 1px solid #389d70;
+
+    &:hover {
+      cursor pointer
+      background: #4abf8a;
+    }
+  }
+}
+</style>
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,6 @@ home: true
 heroImage: /DD.png
 heroText: 黄秤砣的个人网站
 tagline: 基于 vuePress 搭建
-actionText: 进入笔记 →
-actionLink: /note/basic.md
 footer: MIT Licensed
 ---
+<custom-action />
