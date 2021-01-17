c2c8bc63ec9c793fb288b85e0691d395e6856f20
HuangChengtuo <hct564117373@gmail.com>
2021-01-17 23:59:17 +0800

    severless

--- node.js
+++ node.js
@@ -3,10 +3,5 @@ const path = require('path')
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "docs/.vuepress/dist/index.html"))
-})
-
 app.use(express.static(path.join(__dirname, 'docs/.vuepress/dist')))
-const port = 80
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = app
