9599a4a2b0f01992a5d298aec6e107b94719112e
HuangChengtuo <hct564117373@gmail.com>
2021-04-07 00:10:39 +0800

    link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,11 +3,11 @@ module.exports = {
   description: '基于vuePress搭建的个人网站',
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },
+      { text: '首页', link: 'https://www.huangchengtuo.com' },
       { text: '文章', link: '/article/anime.md' },
       { text: '笔记', link: '/note/basic.md' },
       { text: '个人简历', link: '/resume.md' },
-      { text: 'GitHub', link: 'https://github.com/HuangChengtuo' }
+      { text: 'Gitee', link: 'https://gitee.com/HuangChengtuo' }
     ],
     logo: 'https://s1.huangchengtuo.com/img/pureDD.png',
     displayAllHeaders: true,
